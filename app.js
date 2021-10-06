1ec466b3944af98b9b83ce46ef96aa13ed6c2f2b
jatikurniabudi <jatikurniabudi4@gmail.com>
2021-10-06 22:03:52 +0700

    add file index

--- app.js
+++ app.js
@@ -1,12 +1,34 @@
 const http = require("http");
+const fs = require("fs");
+
+const port = 3000;
 
 http
   .createServer((req, res) => {
-    res.write("halo gaes");
-    res.end();
+    res.writeHead(200, {
+      "Content-Type": "text/html",
+    });
+
+    const url = req.url;
+    if (url === "/about") {
+      console.log(url);
+      res.write("Ini halaman about loh");
+      res.end();
+    } else {
+      //   res.write("halo gaes");
+      fs.readFile("./index.html", (err, data) => {
+        if (err) {
+          res.writeHead(404);
+          res.write("file not found");
+        } else {
+          res.write(data);
+        }
+        res.end();
+      });
+    }
   })
-  .listen(3000, () => {
-    console.log("server running in port 3000");
+  .listen(port, () => {
+    console.log(`server running in port ${port}...`);
   });
 
 // const server = http.createServer((req, res) => {
